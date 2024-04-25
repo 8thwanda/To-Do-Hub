fb3db8601890581b6ae036fa3a4c5354282cc559
8thwanda <138054616+8thwanda@users.noreply.github.com>
2024-04-26 01:46:02 +0200

    Update AddToDoScreen.js

--- src/screens/AddToDoScreen.js
+++ src/screens/AddToDoScreen.js
@@ -1,64 +1,76 @@
-//screens/AddToDoScreen.js
+//screens/AddTodoScreen.js
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { SafeAreaView, KeyboardAvoidingView, Platform, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/native';
+import { RichText, Toolbar, useEditorBridge } from '@10play/tentap-editor';
 
 const AddToDoScreen = ({ route }) => {
   const [text, setText] = useState('');
-  const { todo } = route.params || {}; // Extract todo from params
+  const { todo } = route.params || {};
   const navigation = useNavigation();
 
-  // If todo exists (i.e., editing an existing item), set the text state to the existing text
+  // Initialize the 10tap editor
+  const editor = useEditorBridge({
+    autofocus: true,
+    avoidIosKeyboard: true,
+    initialContent: todo ? todo.text : '', // Provide initial content from todo
+  });
+
+ 
+ 
   useEffect(() => {
     if (todo) {
-      setText(todo.text);
+      // Set initial content of the editor to the text of the todo
+      //console.log("Todo Text:", todo.text);
+      editor.setContent(todo.text);
     }
   }, [todo]);
 
-  // Modify addTodo function to handle editing of existing to-do item
   const addTodo = () => {
-    if (text.trim() !== '') {
-      if (todo) {
-        const updatedTodo = { ...todo, text: text }; // Update text of existing todo
-        navigation.navigate('TodoScreen', { newTodo: updatedTodo });
-      } else {
-        const newTodo = { id: Date.now().toString(), text: text };
-        navigation.navigate('TodoScreen', { newTodo: newTodo }); 
+    editor.getText().then((text) => {
+      if (text.trim() !== '') {
+        if (todo) {
+          const updatedTodo = { ...todo, text: text }; // Update text of existing todo
+          navigation.navigate('TodoScreen', { newTodo: updatedTodo });
+        } else {
+          const newTodo = { id: Date.now().toString(), text: text };
+          navigation.navigate('TodoScreen', { newTodo: newTodo }); 
+        }
+        setText('');
       }
-      setText('');
-    }
+    });
   };
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Add a new todo"
-        value={text}
-        onChangeText={setText}
-      />
+    <SafeAreaView style={styles.fullScreen}>
+      <RichText editor={editor} />
+      <Toolbar editor={editor} />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.keyboardAvoidingView}
+      >
+      </KeyboardAvoidingView>
       <TouchableOpacity style={styles.addButton} onPress={addTodo}>
-        <Icon name="plus" size={20} color="white" />
-      </TouchableOpacity>
-    </View>
+          <Icon name="plus" size={20} color="white" />
+        </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  fullScreen: {
     flex: 1,
-    padding: 20,
-    justifyContent: 'flex-end',
   },
-  input: {
-    marginBottom: 20,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    padding: 10,
-    borderRadius: 5,
+  keyboardAvoidingView: {
+    position: 'absolute',
+    width: '100%',
+    bottom: 0,
   },
   addButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
     backgroundColor: 'blue',
     width: 40,
     height: 40,
